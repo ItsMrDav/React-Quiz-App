7884c1aca421d04a0c11214ee77f158af2776034
Davut Simsek <davutt.simsek@gmail.com>
2025-09-19 22:42:05 +0200

    Add questions logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Quiz from './components/quiz';
 import './index.css';
 
 function App() {
   return (
     <div className="app-container">
       <h1>Quiz App</h1>
+      <Quiz />
     </div>
   );
 }
